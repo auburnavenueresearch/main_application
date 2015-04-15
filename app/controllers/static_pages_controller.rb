6ecccb87eb8a57f7dbf74f695a4e836e46ad7118
belozi <robert.belozi.lee@gmail.com>
2015-04-15 13:46:37 -0400

    *fixed

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,10 +5,6 @@ class StaticPagesController < ApplicationController
       @post = current_user.posts.build 
       @feed_items = current_user.feed.paginate(page: params[:page])
     end
-    results = HTTParty.get("http://api.momentfeed.com/pinsync.json?name=Sears&locality=Atlanta&region=GA&api_token=VROUGHASWYWKAWMY")
-    store1 = results.first
-    @name = store1['store_info']['name']
-    @address = store1['store_info']['address']
   end
 
   def team
